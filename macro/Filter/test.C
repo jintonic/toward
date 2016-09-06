30d485d80656dc36f5b48efff8d45077910206dd
Jianchen Li <byljron@gmail.com>
2016-09-06 18:50:07 -0500

    add noise

--- macro/Filter/test.C
+++ macro/Filter/test.C
@@ -6,11 +6,12 @@ void test()
   WF *wf = new WF;
   for (int i=0;i<500;i++)wf->smpl.push_back(0);
   wf->smpl.push_back(0);
-  for(int i=0;i<5;i++)wf->smpl.push_back(i);
+  for(int i=0;i<15;i++)wf->smpl.push_back(i);
   for (int i=0;i<500;i++)wf->smpl.push_back(5);
+  wf->AddNoise(1);
   WF * FT=wf->T2F();
   WF * tr=new WF();
   wf->GetTrapozoidE(-1,-1,tr);
-  for(int i=0;i<100;i++)cout<<FT->smpl[i]<<endl;
+  //for(int i=0;i<100;i++)cout<<FT->smpl[i]<<endl;
   FT->Draw("AC*");
 }
