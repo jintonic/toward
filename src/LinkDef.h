674097b46f146cc319dd4f50f5c0b191f25de3b8
Jing LIU <jing.liu@usd.edu>
2020-11-22 00:53:11 -0600

    removed

--- src/LinkDef.h
+++
@@ -1,6 +0,0 @@
-#ifdef __CLING__
-#pragma link C++ namespace TOWARD;
-#pragma link C++ class TOWARD::Pulse+;
-#pragma link C++ class TOWARD::Waveform+;
-#pragma link C++ class TOWARD::Waveforms+;
-#endif
