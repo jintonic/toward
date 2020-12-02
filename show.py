8b1c297bb54a1d8c032eccadd509317856bd2647
Jing LIU <jing.liu@usd.edu>
2020-12-02 15:33:14 -0600

    binded <Enter> to run_cmd

--- show.py
+++ show.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 from tkinter import Tk, Entry, TOP, BOTTOM, BOTH
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 # handle default matplotlib key bindings
@@ -10,33 +10,31 @@ file = uproot.open('run/0/wave0.root')
 t = file['t']
 b = t.arrays()
 
-fig = Figure(figsize=(5, 4), dpi=100)
+fig = Figure(figsize=(6, 4), dpi=100)
 fig.add_subplot(111).plot(b[b's'][0])
 
 window = Tk()
-window.wm_title("displaying event 0/125 in run 0")
+window.wm_title("event 0/125 in channel 0, run 0")
 
-canvas = FigureCanvasTkAgg(fig, master=window)  # A tk.DrawingArea.
+canvas = FigureCanvasTkAgg(fig, master=window)
 canvas.draw()
 canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
 
-def run_cmd():
-    cmdline.get()
+def run_cmd(event):
+    print("cmd: {}".format(cmdline.get()))
 
 cmdline=Entry(window)
+cmdline.insert(0, "<TAB>: switch in between the canvas and this cmd line")
 cmdline.pack(side=BOTTOM, fill=BOTH)
-cmdline.bind('<Escape>', canvas.get_tk_widget().focus_set())
-cmdline.bind('<Return>', run_cmd())
+# https://stackoverflow.com/questions/47475783
+cmdline.bind('<Return>', run_cmd)
  
 def handle_key_press(event):
     print("you pressed {}".format(event.key))
     if format(event.key) == "q":
         window.quit()    # stop mainloop
         window.destroy() # necessary on Windows to prevent fatal error
-    elif format(event.key) == "escape":
-        cmdline.clear()
-        canvas.get_tk_widget().focus_set()
-    if format(event.key)=="g":
+    elif format(event.key)=="g":
         cmdline.focus_set()
     else:
         key_press_handler(event, canvas)
